f6df9c296f951e114f41cf86fdaa9d1fc6a5aec0
zhufree <zhufree2013@gmail.com>
2022-07-28 14:00:05 +0800

    :rocket: update nonebot, using pipenv, add audio reply

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import nonebot
-from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+from nonebot.adapters.onebot.v11 import Adapter
 # Custom your logger
 # 
 # from nonebot.log import logger, default_format
@@ -14,16 +14,12 @@ from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
 # You can pass some keyword args config to init function
 nonebot.init()
-app = nonebot.get_asgi()
+# app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
-driver.register_adapter("cqhttp", CQHTTPBot)
+driver.register_adapter(Adapter)
 nonebot.load_builtin_plugins()
-nonebot.load_plugin("nonebot_plugin_hanayori") # B站开播提醒插件
-nonebot.load_plugin("nonebot_plugin_epicfree") # epic限免提醒插件
-nonebot.load_plugin("nonebot_plugin_simplemusic") # 点歌插件
-nonebot.load_plugin("nonebot_plugin_analysis_bilibili")
-nonebot.load_plugins("src/plugins")
+# nonebot.load_plugins("src/plugins")
 
 nonebot.load_from_toml("pyproject.toml")
 
@@ -35,4 +31,4 @@ nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
     nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
-    nonebot.run(app="__mp_main__:app")
+    nonebot.run()
--- src/plugins/reaction.py
+++ src/plugins/reaction.py
@@ -1,7 +1,44 @@
-from nonebot import on_command, on_keyword
+from nonebot import on_command, on_keyword, on_regex
 from nonebot.rule import to_me
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_message
+import os, random
+
+judy_matcher = on_regex(r"\s*((?:Judy|朱迪|朱)(?:叫|按钮))|(?:mea +button)\s*")
+qywy_matcher = on_regex(r'温温温|绝绝绝')
+txl_matcher = on_regex(r'探虚陵|txl|探')
+
+def get_audio_path(audio_dir):
+    audio_files = os.listdir(f'audios/{audio_dir}')
+    random_audio = random.choice(audio_files)
+    base_path = os.path.split(os.path.abspath(random_audio))[0]
+    return 'file:///' + base_path + f'\\audios\\{audio_dir}\\' + random_audio
+
+@judy_matcher.handle()
+async def handle_audio(bot: Bot, event: Event):
+    path = get_audio_path('judy')
+    audio_msg = Message(MessageSegment(type='record', data={
+        'file':path}
+    ))
+    await judy_matcher.finish(audio_msg)
+
+@qywy_matcher.handle()
+async def handle_audio(bot: Bot, event: Event):
+    path = get_audio_path('qywy')
+    audio_msg = Message(MessageSegment(type='record', data={
+        'file':path}
+    ))
+    await qywy_matcher.finish(audio_msg)
+
+@txl_matcher.handle()
+async def handle_audio(bot: Bot, event: Event):
+    path = get_audio_path('txl')
+    audio_msg = Message(MessageSegment(type='record', data={
+        'file':path}
+    ))
+    await txl_matcher.finish(audio_msg)
 
 keywords_dict = {
     '嘤': '嘤嘤嘤'
